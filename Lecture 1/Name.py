b0f3ad23517c96a33157695a2913b42932667ff4
Dave <80132827+Dave-code-creater@users.noreply.github.com>
2024-02-21 18:27:25 +0000

    First exercise - Get user input and display it on screen with unit test

---
+++ Lecture 1/Name.py
@@ -0,0 +1,6 @@
+# Check your assignment 
+# 1. Create a variable called "name" and assign it a string that is your name.
+# 2. Print the variable name.
+
+name = "John"
+print(name)
---
+++ Lecture 1/README.md
@@ -0,0 +1,5 @@
+In this lecture, you will ned to finish 5 different problems
+1. Display the name
+2. Performe calculation
+3. Find the change
+4. Print the date
