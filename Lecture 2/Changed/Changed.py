997e124f2445d1e090ccd87c21142d714cc32134
Dave-code-creater <dat981dat@gmail.com>
2025-02-05 14:15:07 -0500

    Re-organize everything into a better order

--- Lecture 2/Changed/Changed.py
+++
@@ -1,5 +0,0 @@
-### This program is a simple example of a function that takes an integer as input and returns an integer as output.
-### The function takes an integer as input and returns as coins and 
-
-def Changed(money: int) -> int:
-    pass
--- Lecture 2/Task/Name/Name.py
+++ Lecture 2/Task/Name/Name.py
@@ -2,5 +2,6 @@
 # 1. Create a variable called "name" and assign it a string that is your name.
 # 2. Print the variable name.
 
-name = 
+# TODO: Put your name so that it can print into the screen
+name = "Dave-code-creater"
 print(name)
